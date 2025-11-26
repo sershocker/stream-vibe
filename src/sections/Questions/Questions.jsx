42e94dcb4ddd7006a1abe244babeffcd8cd78587
ser23 <ksa27023@gmail.com>
2025-11-26 18:02:35 +0300

    accordion component completed

--- src/sections/Questions/Questions.jsx
+++ src/sections/Questions/Questions.jsx
@@ -3,6 +3,7 @@ import classNames from 'classnames'
 import Section from "@/layouts/Section";
 import Button from "@/components/Button";
 import AccordionGroup from "@/components/AccordionGroup";
+import Accordion from "@/components/Accordion";
 
 const Questions = (props) => {
     const {
@@ -37,7 +38,17 @@ const Questions = (props) => {
             }
         >
             <AccordionGroup columns = "2">
-                {questionItems}
+                {questionItems.map((question, index) => (
+                    <Accordion
+                    title={question}
+                    id={`question-${index}`}
+                    name="questions"
+                    isOpen={index === 0}
+                    key={index}
+                    >
+                    <p>StreamVibe is a streaming service that allows you to watch movies and shows on demand.</p>
+                    </Accordion>
+                ))}
             </AccordionGroup>
         </Section>
     )
