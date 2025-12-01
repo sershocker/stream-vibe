4339228fc29aba3f92819784637f51199157a867
ser23 <ksa27023@gmail.com>
2025-12-01 23:57:29 +0300

    Banner component completed

--- src/global.jsx
+++ src/global.jsx
@@ -3,6 +3,7 @@ import { Head } from "minista"
 import Header from "@/layouts/Header";
 import Content from "@/layouts/Content";
 import Footer from "@/layouts/Footer";
+import Banner from "@/sections/Banner";
 
 export default function (props) {
     const { children, title, url, isHeaderFixed } = props;
@@ -19,6 +20,7 @@ export default function (props) {
             <Header url={url} isFixed={isHeaderFixed}/>
             <Content>
                 {children}
+                <Banner />
             </Content>
             <Footer/>
         </>
